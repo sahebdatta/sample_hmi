9f8401a90023bdcd644c8264bd8c275758a2932a
Subhadeep Datta <sahebdatta1996@gmail.com>
2023-05-25 07:22:13 +0530

    initial commit

---
+++ splide.js
@@ -0,0 +1,6 @@
+document.addEventListener( 'DOMContentLoaded', function () {
+    new Splide( '#fullscreen-carousel', {
+          width : '100vw',
+          height: '100vh',
+    } ).mount();
+  } );
